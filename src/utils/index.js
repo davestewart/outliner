4255c4059cf8b8ddd06497fbdefaa0a0ca08ae1e
Dave Stewart <dave@davestewart.co.uk>
2021-09-16 18:12:31 +0100

    Improve logging

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,21 @@ function relPath (path) {
   return path.replace(process.cwd(), '')
 }
 
+function colorize (value) {
+  const text = String(value)
+  if (value === 'skipped' || value === false || value === 0) {
+    return text.grey
+  }
+  if (value === 'updated') {
+    return text.green
+  }
+  if (value === 'error') {
+    return text.red
+  }
+  return value
+}
+
 module.exports = {
   relPath,
+  colorize,
 }
